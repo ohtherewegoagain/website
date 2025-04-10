2cf875a8738d2a59965510c39be15467e33b2b5c
ohtherewegoagain <pooja892892@gmail.com>
2025-04-11 00:21:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/website/',  // <- this must match your GitHub repo name
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  }
+});
